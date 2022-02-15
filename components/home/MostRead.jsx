8faf5c9872d13d4becad5234f4e2a4d8c87915c7
gareginra <40554131+gareginra@users.noreply.github.com>
2022-02-15 04:27:36 +0400

    test14.1

--- components/home/MostRead.jsx
+++ components/home/MostRead.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react/cjs/react";
+import React, { useState, useEffect } from "react";
 import MostReadPost from "./MostReadPost";
 import data from "/data/files/posts.js";
 const MostRead = () => {
