24f91f02ad4f6e683d046cdcd2505d70ea526062
Denis Balandin <denis.b@humblpay.com>
2023-01-14 12:08:41 -0800

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["nprussia.org", "gdb.rferl.org", "region.expert"],
+    domains: ["nprussia.org", "gdb.rferl.org", "region.expert", "sib.fm"],
   },
 };
